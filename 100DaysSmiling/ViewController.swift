f031f6f60d5685ef2aa0191bec2b72056fef46c7
Felipe Florencio Garcia <2136475+felipeflorencio@users.noreply.github.com>
2019-06-21 18:48:25 +0200

    Initial Commit

---
+++ 100DaysSmiling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  100DaysSmiling
+//
+//  Created by Felipe Florencio Garcia on 21/06/19.
+//  Copyright © 2019 Felipe Florencio Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 100DaysSmilingTests/_00DaysSmilingTests.swift
@@ -0,0 +1,34 @@
+//
+//  _00DaysSmilingTests.swift
+//  100DaysSmilingTests
+//
+//  Created by Felipe Florencio Garcia on 21/06/19.
+//  Copyright © 2019 Felipe Florencio Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import _00DaysSmiling
+
+class _00DaysSmilingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
